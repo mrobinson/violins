e0af7f280c6f78cc66742f90f44b29543dbe1d68
Martin Robinson <mrobinson@igalia.com>
2014-04-15 15:17:47 -0700

    First pass at JSON generation for collisions

--- json-for-collisions.py
+++ json-for-collisions.py
@@ -6,18 +6,45 @@ import sqlite3
 import switrs
 import sys
 
+
+def read_all_data_from_database(city_directory):
+    connection = sqlite3.connect(os.path.join(city_directory, "all-collisions.db"))
+    for row in connection.execute('SELECT * FROM collisions;'):
+        switrs.Collision(row)
+    for party in [switrs.Party(row) for row in connection.execute('SELECT * FROM parties;')]:
+        switrs.Collision.collisions[party.collision_id].parties.append(party)
+    for victim in [switrs.Victim(row) for row in connection.execute('SELECT * FROM victims;')]:
+        switrs.Collision.collisions[victim.collision_id].victims.append(victim)
+    connection.close()
+
+def find_all_bike_and_pedestrian_collision():
+    for collision in switrs.Collision.collisions.values():
+        if collision.motor_vehicle_with == 'G' or collision.motor_vehicle_with == 'B':
+            yield collision
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Must specify city directory")
         sys.exit(1)
 
-
-    city_directory = sys.argv[1]
-    connection = sqlite3.connect(os.path.join(city_directory, "all-collisions.db"))
+    read_all_data_from_database(sys.argv[1])
 
     data = []
-    for row in connection.execute('SELECT * FROM collisions WHERE motor_vehicle_with = "G" OR motor_vehicle_with = "B"'):
-        collision = switrs.Collision(row)
-        data.append([collision.latitude, collision.longitude])
+    for collision in find_all_bike_and_pedestrian_collision():
+        victims = []
+        for victim in collision.victims:
+            victims.append({
+                'age': int(victim.age),
+                'sex': victim.sex,
+                'injury': int(victim.degree_of_injury),
+            })
+        data.append({
+            'type': 'bike' if collision.motor_vehicle_with == 'G' else 'pedestrian',
+            'intersection': '{0} and {1}'.format(collision.primary_road, collision.secondary_road),
+            'location': [float(collision.latitude), float(collision.longitude)],
+            'date': collision.date,
+            'time': collision.time,
+            'victims': victims,
+        })
 
     print(json.dumps(data))
